d42f803378400a1dcbcda4a783fa7930aed3e187
Shevchuk-Vadym <lorelliand@gmail.com>
2020-03-11 17:11:52 +0200

    init

--- hw_11/scripts.js
+++ hw_11/scripts.js
@@ -1,9 +1,18 @@
 const button = document.querySelector(".button");
+const noneHello = document.querySelector(".hello");
 
 function myFunction() {
   if (button.style.background === "green") {
     button.style.background = "red";
   } else {
     button.style.background = "green";
+  }
+}
+
+function displayOn() {
+  if (button.style.background === "green") {
+    noneHello.style.display = "block";
+  } else {
+    noneHello.style.display = "none";
   }
 }
