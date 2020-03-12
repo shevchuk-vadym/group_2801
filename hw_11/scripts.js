647975bd7864bdd80556f3ebcfb9aa608d6ae51a
Shevchuk-Vadym <lorelliand@gmail.com>
2020-03-12 18:41:05 +0200

    kkk

--- hw_11/scripts.js
+++ hw_11/scripts.js
@@ -1,18 +1,31 @@
+// const button = document.querySelector(".button");
+// const noneHello = document.querySelector(".hello");
+
+//   function displayOn() {
+//   if (button.style.background === "green") {
+//     noneHello.style.display = "none";
+//     button.style.background = "red";
+//   } else {
+//     noneHello.style.display = "block";
+//     button.style.background = "green";
+//   }
+// }
+
 const button = document.querySelector(".button");
 const noneHello = document.querySelector(".hello");
-
-function myFunction() {
+function t() {
   if (button.style.background === "green") {
+    noneHello.style.display = "block";
     button.style.background = "red";
   } else {
+    noneHello.style.display = "none";
     button.style.background = "green";
   }
 }
-
+function o() {
+  console.log("HUY");
+}
 function displayOn() {
-  if (button.style.background === "green") {
-    noneHello.style.display = "block";
-  } else {
-    noneHello.style.display = "none";
-  }
+  o();
+  t();
 }
