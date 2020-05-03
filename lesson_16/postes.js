d7572704c147df9e106b51536c837993988fefb7
Shevchuk-Vadym <lorelliand@gmail.com>
2020-05-04 01:40:45 +0300

    add text for lesson 16

--- lesson_16/postes.js
+++ lesson_16/postes.js
@@ -25,8 +25,20 @@ class Postes {
       for (let obj of data.list) {
         const ul = document.createElement("ul");
         const img = document.createElement("img");
+        const p = document.createElement("p");
+        const h3 = document.createElement("h3");
+        const h5 = document.createElement("h5");
+
+        p.textContent = obj.description;
+        h3.textContent = obj.title;
+        h5.textContent = obj.genre;
         img.src = obj.preview.high;
+
+        ul.append(h3);
         ul.append(img);
+        ul.append(p);
+        ul.append(h5);
+
         this.rootElement.append(ul);
       }
     };
